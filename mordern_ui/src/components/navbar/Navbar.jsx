a688bde342ecec302c2f2b9ee4d82566954733c8
unknown <achonwaechris@outlook.com>
2022-04-28 06:19:02 +0100

    fex header

--- mordern_ui/src/components/navbar/Navbar.jsx
+++ mordern_ui/src/components/navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {RiMenu3lin3, RiCloseLine} from 'react-icons/r1'
+import {RiMenu3lin3, RiCloseLine} from 'react-icons/ri'
 import './navbar.css'
+import logo from '../../assets/logo.svg';
 const Navbar = () => {
   return (
-    <div className='gp3__navbar'>
+    <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className="gpt3__navbar-link_logo">
           <img src='logo' alt='logo'/>
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href='#home'>Home </a></p>
+        <div>
       </div>
-    </div>
+    </div>  
   )
 }
 
