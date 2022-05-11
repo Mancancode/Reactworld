01279aa6c73371429c96d0bd9067a81a8b3fc72d
unknown <achonwaechris@outlook.com>
2022-05-11 18:35:31 +0100

    fixed what is gpt

--- mordern_ui/src/components/cta/CTA.jsx
+++ mordern_ui/src/components/cta/CTA.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './cta.css'
 const CTA = () => {
   return (
-    <div>cta</div>
+    <div className='cta_gpt3'>Hello nthis is the test of call to action </div>
   )
 }
 
